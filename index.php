f9a2773e633d1f8de28a5b13ebadfe089220615e
unknown <luccasdrapala@gmail.com>
2022-11-12 17:29:38 -0300

    rotas com parametros opcionais

--- index.php
+++ index.php
@@ -5,15 +5,26 @@ require 'vendor/autoload.php';
 //necessário para criar as rotas
 $app = new \Slim\App;
 
-
-$app->get('/rota1', function(){
+//nesse caso o parametro {} é obrigatório
+$app->get('/rota1/{parametro}', function(){
 
     echo 'teste rota1';
 });
 
-$app->get('/rota2/{id}', function(){
+//nesse caso o parametro {} é opcional
+$app->get('/rota2[/{id}]', function($request, $response){
 
-    echo 'teste rota2';
+    $id = $request->getAttribute('id');
+    echo 'id '. $id;
+});
+
+//2 parametros opcionais, porém se 1 for passado sempre será o primeiro(parece gambiarra)
+$app->get('/rota3[/{parametro1}[/{parametro2}]]', function($request, $response){
+
+    $p1 = $request->getAttribute('parametro1');
+    $p2 = $request->getAttribute('parametro2');
+    echo 'Parametro 1: '. $p1;
+    echo '<br>Parametro 2: '. $p2;
 });
 
 $app->run();
