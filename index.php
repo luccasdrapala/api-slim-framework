a9e2f224d1a7e6a5a4c6b6a2687024daa3ba871a
unknown <luccasdrapala@gmail.com>
2022-11-13 12:03:33 -0300

    configurando metodos dos verbos POST, PUT e DELETE

--- index.php
+++ index.php
@@ -7,11 +7,49 @@ require 'vendor/autoload.php';
 
 $app = new \Slim\App;
 
-//padrão Psr-7
+//padrão Psr-7 - verbo GET
 $app->get('/rota/{id}', function(Request $request, Response $response){
 
     $response->getBody()->write('Retorno com sucesso');
     return $response;
+});
+
+//padrão Psr-7 - verbo POST
+$app->post('/usuario/adiciona', function(Request $request, Response $response){
+
+    //recuperando dados do post(formulario)
+    $post = $request->getParsedBody();
+
+    //normalmente estaria aqui a logica do INSERT 
+
+    return $response->getBody()->write($post['email']);
+});
+
+//padrão Psr-7 - verbo PUT
+$app->put('/usuario/atualiza/{id}', function(Request $request, Response $response){
+
+    //recuperando dados do post(formulario)
+    $post = $request->getParsedBody();
+    $id = $post['id'];
+    $nome = $post['nome'];
+    $email = $post['email'];
+
+    //logica para UPDATE no banco
+
+    return $response->getBody()->write('Usuário: '. $id.'-'.$nome.'-'.$email.' - Atualizado com sucesso');
+
+});
+
+//padrão Psr-7 - verbo DELETE
+$app->delete('/usuario/remove/{id}', function(Request $request, Response $response){
+
+    //recuperando dados do post(formulario)
+    $id = $request->getAttribute('id');
+
+    //logica para DELETE no banco
+
+    return $response->getBody()->write('Usuário: '. $id.' - Deletado com sucesso');
+
 });
 
  $app->run();   
