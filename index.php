f69a400ea623cfe3f36fb70880390d98f3a25def
unknown <timafra2@mallon.com.br>
2022-11-14 10:20:00 -0300

    injeção de dependencias com container pimple

--- index.php
+++ index.php
@@ -7,49 +7,20 @@ require 'vendor/autoload.php';
 
 $app = new \Slim\App;
 
-//padrão Psr-7 - verbo GET
-$app->get('/rota/{id}', function(Request $request, Response $response){
+class Servico{
+    //somente para fins didaticos
+}
 
-    $response->getBody()->write('Retorno com sucesso');
-    return $response;
+/**container pimple */
+$container = $app->getContainer();
+$container['servico'] = function(){
+    return new Servico;
+}
+
+$app->get('/servico', function(Request $request, REsponse $response){
+
+    $servico = $this->get('servico');
+    var_dump($servico);
 });
 
-//padrão Psr-7 - verbo POST
-$app->post('/usuario/adiciona', function(Request $request, Response $response){
-
-    //recuperando dados do post(formulario)
-    $post = $request->getParsedBody();
-
-    //normalmente estaria aqui a logica do INSERT 
-
-    return $response->getBody()->write($post['email']);
-});
-
-//padrão Psr-7 - verbo PUT
-$app->put('/usuario/atualiza/{id}', function(Request $request, Response $response){
-
-    //recuperando dados do post(formulario)
-    $post = $request->getParsedBody();
-    $id = $post['id'];
-    $nome = $post['nome'];
-    $email = $post['email'];
-
-    //logica para UPDATE no banco
-
-    return $response->getBody()->write('Usuário: '. $id.'-'.$nome.'-'.$email.' - Atualizado com sucesso');
-
-});
-
-//padrão Psr-7 - verbo DELETE
-$app->delete('/usuario/remove/{id}', function(Request $request, Response $response){
-
-    //recuperando dados do post(formulario)
-    $id = $request->getAttribute('id');
-
-    //logica para DELETE no banco
-
-    return $response->getBody()->write('Usuário: '. $id.' - Deletado com sucesso');
-
-});
-
- $app->run();   
+$app->run();   
