c9be4daef7c7844ea989d0263261824f12a6774a
unknown <luccasdrapala@gmail.com>
2022-11-16 19:31:36 -0300

    recuperando request do container dentro do objeto

--- src/app/controllers/Home.php
+++ src/app/controllers/Home.php
@@ -4,7 +4,16 @@ namespace App\controllers;
 
 class Home{
 
+    protected $container;
+
+    public function __construct($container){
+        $this->container = $container;
+    }
+
     public function index($request, $response){
+
+        $requisicao = $this->container->get('request');
+        var_dump($requisicao);
         return $response->write('teste do index');
     }
 
