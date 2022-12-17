52f5723f21510bdb38dcb09b1df680cde623e88f
unknown <luccasdrapala@gmail.com>
2022-12-17 19:34:52 -0300

    adicionando put para atualizar produto pelo id

--- src/routes/produtos.php
+++ src/routes/produtos.php
@@ -8,12 +8,21 @@ use App\Models\Produto;
 //routes
 $app->group('/api/v1', function(){
 
+    //lista todos os produtos
     $this->get('/produtos/lista', function(Request $request, Response $response){
         
         $produtos = Produto::get();
         return $response->withJson($produtos);
     });
+
+    //lista produto pelo id
+    $this->get('/produtos/lista/{id}', function(Request $request, Response $response, $args){
+        
+        $produto = Produto::findOrFail($args['id']);
+        return $response->withJson($produto);
+    });
     
+    //adiciona produto
     $this->post('/produtos/adiciona', function(Request $request, Response $response){
         
         $dados = $request->getParsedBody(); //captura o post
@@ -21,10 +30,12 @@ $app->group('/api/v1', function(){
         return $response->withJson($produto);
     });
 
-    $this->get('/produtos/lista/{id}', function(Request $request, Response $response, $args){
+    //atualiza produto pelo id
+    $this->put('/produtos/atualiza/{id}', function(Request $request, Response $response, $args){
         
-        $produto = Produto::findOrFail($args['id']);
-        return $response->withJson($produto);
+        $dados = $request->getParsedBody();
+        $produto = Produto::findOrFail ($args['id']);
+        $produto->update( $dados );
+        return $response->withJson( $produto );
     });
-
 });
