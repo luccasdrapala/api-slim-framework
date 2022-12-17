c5431d8daf506793d7c6073fe6c3674a1ba8243f
unknown <luccasdrapala@gmail.com>
2022-12-17 19:06:42 -0300

    configurando model (eloquent), para receber deternmindas campos

--- App/Models/Produto.php
+++ App/Models/Produto.php
@@ -5,7 +5,10 @@ use illuminate\Database\Eloquent\Model;
 
 class Produto extends Model
 {
-
+    protected $fillable = [ //preenchivel
+        'titulo', 'descricao', 'preco', 'fabricante',
+        'created_at', 'updated_at'
+    ];
 }
 
 ?>
